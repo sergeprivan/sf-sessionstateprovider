bea7df268eb5487c35a23af59f8e225ca210f6f2
Serge Privan <seprivan@microsoft.com>
2019-12-17 18:00:09 +0100

    fix

--- Microsoft.Extensions.Caching.ServiceFabric.Test/Startup.cs
+++ Microsoft.Extensions.Caching.ServiceFabric.Test/Startup.cs
@@ -46,7 +46,7 @@ namespace Microsoft.Extensions.Caching.ServiceFabric.Test
                 options.Cookie.IsEssential = true;
             });
 
-            //test 14
+            //test 1411
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
